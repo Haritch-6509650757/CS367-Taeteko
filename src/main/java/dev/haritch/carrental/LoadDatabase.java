abd5cd7986ba3c8750ccb284433cda92b461fad7
wuttipat6509650716 <wuttipat.pip@dome.tu.ac.th>
2025-05-01 22:10:30 +0700

    add xx888 into loaddata

--- src/main/java/dev/haritch/carrental/LoadDatabase.java
+++ src/main/java/dev/haritch/carrental/LoadDatabase.java
@@ -14,7 +14,7 @@ public class LoadDatabase {
     CommandLineRunner intiDatabase(WarehouseRepository repository){
         return args -> {
             log.info("Loading "+repository.save(new CarStorage("Sedan", "Toyota", "AB1000", "1st Floor", "Camry", "White", true, "Customer not showup", "15000KM", null, null, 50)));
-            log.info("Loading "+repository.save(new CarStorage("Suv", "Toyota", "ขค2345", "2st floor", "Corolla Cross", "Grey", true, "not Rental", "5000km", null, null, 60)));
+            log.info("Loading "+repository.save(new CarStorage("Suv", "Toyota", "XX888", "2st floor", "Corolla Cross", "Grey", true, "not Rental", "5000km", null, null, 60)));
             log.info("Loading"+repository.save(new CarStorage("Suv", "Porsche", "PS1234", "3rd Floor", "Cayenne", "White", true, "not Rental", "1000km", null, null, 100)));
             log.info("Loading"+repository.save(new CarStorage("Sedan", "BMW", "KS9099", "3rd Floor", "M340i", "Black", true, "not Rental", "4700km", null, null, 80)));
             log.info("Loading"+repository.save(new CarStorage("Sedan", "BMW", "AATEST", "3rd Floor", "M340i", "Black", true, "not Rental", "4700km", null, null, 80)));
