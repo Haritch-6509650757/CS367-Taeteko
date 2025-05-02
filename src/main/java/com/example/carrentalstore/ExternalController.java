931ae6742e57defc31b41c4b6e1ea8aeb6d8b016
wuttipat6509650716 <wuttipat.pip@dome.tu.ac.th>
2025-05-02 17:04:11 +0700

    can u edit my car info

--- src/main/java/com/example/carrentalstore/ExternalController.java
+++ src/main/java/com/example/carrentalstore/ExternalController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -36,4 +37,15 @@ public class ExternalController {
         return ResponseEntity.ok(text);
     }
 
+    @GetMapping("/call-info")
+    public ResponseEntity<?> callInfoFrom9090(@RequestParam String carPlateNumber) {
+        String url = urlWarehouse + "/warehouse/info?carPlateNumber=" + carPlateNumber;
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+            return ResponseEntity.ok(response.getBody());
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body("Car not found");
+        }
+
+    }
 }
